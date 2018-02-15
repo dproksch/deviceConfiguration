4b8712d19014f244f09c943553b6eac2bf59a1ab
David Proksch <proksch@us.ibm.com>
2018-02-15 10:20:47 -0500

    update swizzle

--- swizzleSshd.sh
+++ swizzleSshd.sh
@@ -18,6 +18,10 @@ mv /etc/ssh/sshd_config.new /etc/ssh/sshd_config
 echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
 echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
+echo "Ciphers aes128-ctr,aes192-ctr,aes256-ctr"  >> /etc/ssh/sshd_config
+echo "MACs hmac-sha1,umac-64@openssh.com,hmac-ripemd160"  >> /etc/ssh/sshd_config
+echo " " >> /etc/ssh/sshd_config
+echo "Port 4422 " >> /etc/ssh/sshd_config
 
 
 #
